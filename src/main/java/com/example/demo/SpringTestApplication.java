5fdb789e43fd5b9bb19282718912913a0c5b897b
uekizzz8425151 <147139010+uekizzz8425151@users.noreply.github.com>
2023-11-08 17:00:55 +0900

    テスト

--- src/main/java/com/example/demo/SpringTestApplication.java
+++ src/main/java/com/example/demo/SpringTestApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringTestApplication {
 
+	//テスト
 	public static void main(String[] args) {
 		SpringApplication.run(SpringTestApplication.class, args);
 	}

091ebb68455589f18fc435ee124ca9b61d67b5bc
uekizzz8425151 <147139010+uekizzz8425151@users.noreply.github.com>
2023-11-08 17:13:52 +0900

    test

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/")
@@ -8,5 +9,8 @@ public class Controller {
 	public String start() {
 		return "/index";
 	}
-	
+	@GetMapping("main")
+	public String end() {
+		return "/main";
+	}
 }
